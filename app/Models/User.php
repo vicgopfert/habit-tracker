759660e8bc04d065dab8a6e9dd0a1dda93812d3f
Victor Gopfert <gopfert.web@gmail.com>
2026-05-11 14:11:29 -0300

    feat(User): setup migration/Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,11 +11,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+#[Hidden(['password'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
 
     /**
      * Get the attributes that should be cast.
@@ -25,7 +25,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }}</title>
-    @vite('resource/css/app.css')
+    @vite('resources/css/app.css')
 </head>
 
 <body class="bg-[#FFEDD6]">
