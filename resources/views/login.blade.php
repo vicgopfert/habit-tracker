d40b97307485d8e8d1d2084a02285ea8ebf83571
Victor Gopfert <gopfert.web@gmail.com>
2026-05-11 14:34:26 -0300

    feat(Auth): add login functionality and user session management

---
+++ resources/views/login.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <main class="py-10">
+        <div>
+            <h1>Login</h1>
+        </div>
+    </main>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\SiteController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SiteController::class, 'index']);
+
+Route::get('/login', [LoginController::class, 'index']);
