00cb6166bdd19fc15980752f23675417a984c5c3
Victor Gopfert <gopfert.web@gmail.com>
2026-05-11 11:53:47 -0300

    feat: setup tailwindcss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,15 @@
-<h1>Welcome to the Home Page</h1>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name') }}</title>
+    @vite('resource/css/app.css')
+</head>
+<body>
+    <h1 class="text-3xl font-bold underline text-green-500">
+        Home Page
+    </h1>
+    
+</body>
+</html>
