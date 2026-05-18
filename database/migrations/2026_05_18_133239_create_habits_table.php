4d413c468241f29da8bd0cd97e3b9485a1e0295c
Victor Gopfert <gopfert.web@gmail.com>
2026-05-18 10:46:41 -0300

    feat(Habit/HabitLog): create model/migration

---
+++ database/migrations/2026_05_18_133239_create_habits_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('habits', function (Blueprint $table) {
+            $table->id();
+            $table
+                ->foreignIdFor(User::class)
+                ->constrained()
+                ->cascadeOnDelete();
+            $table
+                ->string('name')
+                ->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('habits');
+    }
+};
